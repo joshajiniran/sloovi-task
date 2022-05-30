8318a1e4fe44739e4f0c27a74b3da157bdfe9a8b
Josh Ajiniran <josuajiniran@gmail.com>
2022-05-30 22:01:15 +0100

    fix pytest warnings, add db logic for test

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,6 +1,10 @@
 import pytest
 
 from main import create_app
+from mongoengine import connect
+from mongoengine.connection import _get_db
+
+connect("test", alias="test")
 
 
 @pytest.fixture()
@@ -15,6 +19,11 @@ def client(app):
     client = app.test_client()
     with app.app_context():
         yield client
+        # drop collection after running client
+        # this fixture should run per function and drop any record before the next function
+        db = _get_db()
+        db.drop_collection("user")
+        db.drop_collection("template")
 
 
 @pytest.fixture()
